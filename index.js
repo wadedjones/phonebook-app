0aee0d0c2f4521f857f62c66533e28e9961d3df4
Wade Jones <wadedjones@gmail.com>
2023-03-08 19:21:54 -0800

    added scripts for deploying and app use express static

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 morgan.token('content', function getContent (req) {
