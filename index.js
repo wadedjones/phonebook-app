952e3954faa411023408383a29c93854360d29b6
Wade Jones <wadedjones@gmail.com>
2023-03-10 19:46:32 -0800

    uibuild

--- index.js
+++ index.js
@@ -30,16 +30,17 @@ app.get('/info', (request, response) => {
     )
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         response.json(person)
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    //const duplicatePerson = phonebook.find(p => p.name === body.name)
-    //console.log(body)
+    //const duplicatePerson = Person.findOne(p => p.name === body.name)
+    console.log(duplicatePerson)
 
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -53,7 +54,6 @@ app.post('/api/persons', (request, response) => {
         })
     }
     */
-
     const person = new Person ({
         name: body.name,
         number: body.number,
@@ -63,10 +63,26 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(p => p.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+   Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -74,6 +90,17 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
